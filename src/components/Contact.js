8152c957055c80108025f08b34f14b2d04b4b468
Nasir Cross-Griifin <nasircrossgriffin@gmail.com>
2024-11-12 14:08:44 -0500

    Contact page (fronted) is done!

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,22 @@
-import './Projects.css';
+import './Contact.css';
 import React, { useState, useCallback } from 'react';
 
 function Contact() {
     return (
         <div className="Contact">
-            <container>
-                { /* Contact form will go here */ }
-            </container>
+            <div>
+                <form method='POST'>
+                    <label htmlFor='company'>Company</label>
+                    <input type="text" name="company" id="company"/>
+                    <label htmlFor='email'>Email</label>
+                    <input type="text" name="email" id="email"/>
+                    <label htmlFor='phone number'>Phone Number</label>
+                    <input type="text" name="phone number" id="number"/>
+                    <label htmlFor="message">Message</label>
+                    <input type="text" name="message" id="message"/>
+                    <input type="submit"/>
+                </form>
+            </div>
         </div>
     );
 }
